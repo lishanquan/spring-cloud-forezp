17389f1d018fc8ea0ef8d736e49244f602725b6b
lishanquan <991887313@qq.com>
2019-07-23 14:13:29 +0800

    add chapter8-7 source code

---
+++ eureka-monitor-client/src/main/java/com/daniel/eurekamonitorclient/EurekaMonitorClientApplication.java
@@ -0,0 +1,15 @@
+package com.daniel.eurekamonitorclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@SpringBootApplication
+public class EurekaMonitorClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMonitorClientApplication.class, args);
+    }
+
+}
