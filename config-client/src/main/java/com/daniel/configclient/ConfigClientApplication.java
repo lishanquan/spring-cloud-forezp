d5398bf6fa56875808d9e87deeba04d1723705ff
lishanquan <991887313@qq.com>
2019-07-23 19:55:12 +0800

    add chapter10-1 source code

---
+++ config-client/src/main/java/com/daniel/configclient/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.daniel.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping("foo")
+    public String hi(){
+        return foo;
+    }
+
+}
