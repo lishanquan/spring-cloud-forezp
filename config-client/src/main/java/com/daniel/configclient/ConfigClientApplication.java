fed495a546cb9977a2355da5cab375b0ec06ed6b
lishanquan <991887313@qq.com>
2019-07-24 19:34:41 +0800

    add chapter10-3 source code

--- config-client/src/main/java/com/daniel/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/daniel/configclient/ConfigClientApplication.java
@@ -3,9 +3,11 @@ package com.daniel.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
